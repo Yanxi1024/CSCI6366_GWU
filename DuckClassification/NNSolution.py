f3accbcd08c572d0a478c6f96d4d0101ea52687e
Ruiyang Chen <im.ruiyangchen@gmail.com>
2024-03-29 11:23:19 -0400

    Update test method

--- DuckClassification/NNSolution.py
+++ DuckClassification/NNSolution.py
@@ -40,24 +40,41 @@ for epoch in range(EPCHO):
         print("[%d, %5d] loss: %.3f" % (epoch + 1, index + 1, running_loss))
         running_loss = 0.0
 
+print("Training finished.")
+
 model.eval()
 
+# with torch.no_grad():
+#     for index, data in enumerate(test_loader):
+#         inputs, _ = data
+#         outputs = model(inputs)
+#         _, predictions = torch.max(outputs, 1)
+#
+#         # Get the image names and predictions
+#         # batch_start_index = index * test_loader.batch_size
+#         # batch_end_index = batch_start_index + len(batch)
+#         # image_names = []
+#         # for i in range(batch_start_index, batch_end_index):
+#         #     dataset_index, sample_index = test_set.cumulative_sizes.index(i), i
+#         #     if dataset_index > 0:
+#         #         sample_index -= test_set.cumulative_sizes[dataset_index - 1]
+#         #     image_names.append(test_set.datasets[dataset_index].imagesNameList[sample_index])
+#
+#         # Print image names and predictions
+#         for image_name, prediction in zip(image_names, predictions):
+#             print(f"{image_name}: {'Duck' if prediction.item() == 1 else 'Not Duck'}")
+
+
+# Test
 with torch.no_grad():
-    for index, data in enumerate(test_loader):
-        batch, _ = data
-        outputs = model(batch)
-        _, predictions = torch.max(outputs, 1)
+    correct = 0
+    total = 0
+    for data in test_loader:
+        inputs, labels = data
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-        # 获取当前批次中每个图像的文件名
-        batch_start_index = index * test_loader.batch_size
-        batch_end_index = batch_start_index + len(batch)
-        image_names = []
-        for i in range(batch_start_index, batch_end_index):
-            dataset_index, sample_index = test_set.cumulative_sizes.index(i), i
-            if dataset_index > 0:
-                sample_index -= test_set.cumulative_sizes[dataset_index - 1]
-            image_names.append(test_set.datasets[dataset_index].imagesNameList[sample_index])
-
-        # 打印文件名和预测结果
-        for image_name, prediction in zip(image_names, predictions):
-            print(f"{image_name}: {'Duck' if prediction.item() == 1 else 'Not Duck'}")
+accuracy = 100 * correct / total
+print(f"Accuracy on the duck test set: {accuracy:.2f}%")
